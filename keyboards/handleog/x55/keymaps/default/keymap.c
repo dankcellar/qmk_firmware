f267ae9f2c1a72264c17fdb38d4e8d1f32c885a8
Eric Downey <edowney29@gmail.com>
2020-04-04 21:36:32 -0400

    Update keymap.c

--- keyboards/handleog/x55/keymaps/default/keymap.c
+++ keyboards/handleog/x55/keymaps/default/keymap.c
@@ -15,33 +15,31 @@
  */
 #include QMK_KEYBOARD_H
 
-enum custom_keycodes {
-    _BL = 0,
-    _FL,
-    SHRUG,
-    DISFACE,
-    FU,
-    TFLIP,
-    TFLIP2,
-    SAD_EYES,
-    TPUT,
-    HAPPYFACE,
-    HEARTFACE,
-    CLOUD,
-    CHANFACE,
-    CMDCLEAR
-};
+// enum custom_keycodes {
+//     SHRUG,
+//     DISFACE,
+//     FU,
+//     TFLIP,
+//     TFLIP2,
+//     SAD_EYES,
+//     TPUT,
+//     HAPPYFACE,
+//     HEARTFACE,
+//     CLOUD,
+//     CHANFACE,
+//     CMDCLEAR
+// };
 
 // enum custom_keycodes { PING = SAFE_RANGE, TURNOFF, TURNON, H_11, H_12, H_13, H_14, H_15, H_21, H_22, H_23, H_24, H_25, H_31, H_32, H_33, H_34, H_35, H_41, H_42, H_43, H_44, H_45, H_51, H_52, H_53, H_54, H_55 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_BL] = LAYOUT_X55(/* Base */
+    [0] = LAYOUT_X55(/* Base */
                         KC_GESC, KC_1, KC_2, KC_3, KC_4,
                         KC_TAB,  KC_Q, KC_W, KC_E, KC_R,
                         KC_CAPS, KC_A, KC_S, KC_D, KC_F,
                         KC_LSFT, KC_Z, KC_X, KC_C, KC_V,
-                        KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, MO(_FL)),
-    [_FL] = LAYOUT_X55(/* Function */
+                        KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, MO(1)),
+    [1] = LAYOUT_X55(/* Function */
                         CMDCLEAR, KC_NO,  KC_NO,     KC_NO,     RESET,
                         SHRUG,    CLOUD,  HAPPYFACE, HEARTFACE, DISFACE,
                         TFLIP,    TFLIP2, KC_NO,     KC_NO,     KC_NO,
