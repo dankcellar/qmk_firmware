6597450e78b1c11abdab830d6dabb117e6bda5c7
Eric Downey <edowney29@gmail.com>
2020-12-31 17:36:11 -0500

    Update keymap.c

--- keyboards/handleog/x55/keymaps/default/keymap.c
+++ keyboards/handleog/x55/keymaps/default/keymap.c
@@ -273,8 +273,10 @@ bool process_record_user(uint16_t keycode, keyrecord_t * record) {
   return true;
 }
 
-void matrix_init_user(void) {
-    rgb_matrix_set_color_all(255, 255, 255);
-}
+void matrix_init_user(void) {}
 
 void matrix_scan_user(void) {}
+
+void keyboard_post_init_user(void) {
+    rgb_matrix_set_color_all(255, 255, 255)
+}
