ecf953e191997351ce4df77309b3dee39efe55bd
Eric Downey <edowney29@gmail.com>
2021-04-02 16:29:21 -0400

    Update rules.mk

--- keyboards/handleog/x55/rules.mk
+++ keyboards/handleog/x55/rules.mk
@@ -8,7 +8,7 @@ MCU = atmega32u4
 #   LUFA DFU     lufa-dfu
 #   QMK DFU      qmk-dfu
 #   atmega32a    bootloadHID
-#BOOTLOADER = atmel-dfu
+BOOTLOADER = atmel-dfu
 
 # Build Options
 #   change yes to no to disable
