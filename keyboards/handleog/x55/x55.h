f2548c17a35612a029e6ee19a6852aa56ccebf24
Eric Downey <edowney29@gmail.com>
2021-04-09 23:07:56 -0400

    Update x55.h

--- keyboards/handleog/x55/x55.h
+++ keyboards/handleog/x55/x55.h
@@ -26,7 +26,7 @@
  * represents the switch matrix.
  */
 
-// #define LAYOUT_X55( \
+// #define LAYOUT_X67( \
 //     k00, k01, k02, k03, k04, \
 //     k10, k11, k12, k13, k14, \
 //     k20, k21, k22, k23, k24, \
