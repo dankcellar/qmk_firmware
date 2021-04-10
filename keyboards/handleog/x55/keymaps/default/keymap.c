8d41d0415d4606b6f413723eaa5e4c5b7cc67127
Eric Downey <edowney29@gmail.com>
2021-04-09 23:06:43 -0400

    idk

--- keyboards/handleog/x55/keymaps/default/keymap.c
+++ keyboards/handleog/x55/keymaps/default/keymap.c
@@ -46,23 +46,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
         KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_TRNS),
 
-    // [2] = LAYOUT_X55(/* Custom */
-    //     H_11, H_12, H_13, H_14, H_15,
-    //     H_21, H_22, H_23, H_24, H_25,
-    //     H_31, H_32, H_33, H_34, H_35,
-    //     H_41, H_42, H_43, H_44, H_45,
-    //     H_51, H_52, H_53, H_54, H_55)
+    [2] = LAYOUT_X55(/* Custom */
+        H_11, H_12, H_13, H_14, H_15,
+        H_21, H_22, H_23, H_24, H_25,
+        H_31, H_32, H_33, H_34, H_35,
+        H_41, H_42, H_43, H_44, H_45,
+        H_51, H_52, H_53, H_54, H_55)
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t * record) {
-//   switch (keycode) {
-//   case PING:
-//     if (record -> event.pressed) {
-//       SEND_STRING("ping");
-//     } else {
-//       SEND_STRING("pong");
-//     }
-//     break;
+  switch (keycode) {
+  case PING:
+    if (record -> event.pressed) {
+      SEND_STRING("ping");
+    } else {
+      SEND_STRING("pong");
+    }
+    break;
     /** Custom keycode macros */
     // case H_11:
     //   if (record -> event.pressed) {
@@ -269,7 +269,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t * record) {
     //     lastPressed = KC_Z;
     //   }
     //   break;
-//   }
+  }
   return true;
 }
 
--- keyboards/handleog/x55/rules.mk
+++ keyboards/handleog/x55/rules.mk
@@ -31,5 +31,5 @@ BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 AUDIO_ENABLE = no           # Audio output on port C6
 FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
 
-RGB_MATRIX_ENABLE = yes
-RGB_MATRIX_DRIVER = IS31FL3731
+# RGB_MATRIX_ENABLE = yes
+# RGB_MATRIX_DRIVER = IS31FL3731
