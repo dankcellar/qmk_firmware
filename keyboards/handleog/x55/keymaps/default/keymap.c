330e5529fd576ea97c8c23ea432f1d539988b3f0
Eric Downey <edowney29@gmail.com>
2021-07-31 12:10:20 -0400

    custom

--- keyboards/handleog/x55/keymaps/default/keymap.c
+++ keyboards/handleog/x55/keymaps/default/keymap.c
@@ -2,11 +2,13 @@
 
 enum custom_keycodes { PING = SAFE_RANGE, TURNOFF, TURNON, H_11, H_12, H_13, H_14, H_15, H_21, H_22, H_23, H_24, H_25, H_31, H_32, H_33, H_34, H_35, H_41, H_42, H_43, H_44, H_45, H_51, H_52, H_53, H_54, H_55 };
 
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {[0] = LAYOUT_X55(KC_GESC, KC_1, KC_2, KC_3, KC_4, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, MO(1)),
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-                                                              [1] = LAYOUT_X55(RGB_MOD, RGB_M_P, RGB_TOG, XXXXXXX, XXXXXXX, RGB_HUD, RGB_HUI, XXXXXXX, XXXXXXX, XXXXXXX, RGB_SAD, RGB_SAI, XXXXXXX, XXXXXXX, XXXXXXX, RGB_VAD, RGB_VAI, XXXXXXX, XXXXXXX, XXXXXXX, RESET, PING, XXXXXXX, XXXXXXX, _______),
+    [0] = LAYOUT_X55(H_11, H_12, H_13, H_14, H_15, H_21, H_22, H_23, H_24, H_25, H_31, H_32, H_33, H_34, H_35, H_41, H_42, H_43, H_44, H_45, H_51, H_52, H_53, H_54, H_55),
 
-                                                              [2] = LAYOUT_X55(H_11, H_12, H_13, H_14, H_15, H_21, H_22, H_23, H_24, H_25, H_31, H_32, H_33, H_34, H_35, H_41, H_42, H_43, H_44, H_45, H_51, H_52, H_53, H_54, H_55)};
+    [2] = LAYOUT_X55(KC_GESC, KC_1, KC_2, KC_3, KC_4, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, MO(1)),
+
+    [1] = LAYOUT_X55(RGB_MOD, RGB_M_P, RGB_TOG, XXXXXXX, XXXXXXX, RGB_HUD, RGB_HUI, XXXXXXX, XXXXXXX, XXXXXXX, RGB_SAD, RGB_SAI, XXXXXXX, XXXXXXX, XXXXXXX, RGB_VAD, RGB_VAI, XXXXXXX, XXXXXXX, XXXXXXX, RESET, PING, XXXXXXX, XXXXXXX, _______)};
 
 bool process_record_user(uint16_t keycode, keyrecord_t* record) {
     switch (keycode) {
@@ -17,110 +19,135 @@ bool process_record_user(uint16_t keycode, keyrecord_t* record) {
                 SEND_STRING("pong");
             }
             return false;
-            break;
         /** Custom keycode macros */
         case H_11:
             if (record->event.pressed) {
+                SEND_STRING("H_11");
             }
-            break;
+            return false;
         case H_12:
             if (record->event.pressed) {
+                SEND_STRING("H_12");
             }
-            break;
+            return false;
         case H_13:
             if (record->event.pressed) {
+                SEND_STRING("H_13");
             }
-            break;
+            return false;
         case H_14:
             if (record->event.pressed) {
+                SEND_STRING("H_14");
             }
-            break;
+            return false;
         case H_15:
             if (record->event.pressed) {
+                SEND_STRING("H_15");
             }
-            break;
+            return false;
         case H_21:
             if (record->event.pressed) {
+                SEND_STRING("H_21");
             }
-            break;
+            return false;
         case H_22:
             if (record->event.pressed) {
+                SEND_STRING("H_22");
             }
-            break;
+            return false;
         case H_23:
             if (record->event.pressed) {
+                SEND_STRING("H_23");
             }
-            break;
+            return false;
         case H_24:
             if (record->event.pressed) {
+                SEND_STRING("H_24");
             }
-            break;
+            return false;
         case H_25:
             if (record->event.pressed) {
+                SEND_STRING("H_25");
             }
-            break;
+            return false;
         case H_31:
             if (record->event.pressed) {
+                SEND_STRING("H_31");
             }
-            break;
+            return false;
         case H_32:
             if (record->event.pressed) {
+                SEND_STRING("H_32");
             }
-            break;
+            return false;
         case H_33:
             if (record->event.pressed) {
+                SEND_STRING("H_33");
             }
-            break;
+            return false;
         case H_34:
             if (record->event.pressed) {
+                SEND_STRING("H_34");
             }
-            break;
+            return false;
         case H_35:
             if (record->event.pressed) {
+                SEND_STRING("H_35");
             }
-            break;
+            return false;
         case H_41:
             if (record->event.pressed) {
+                SEND_STRING("H_41");
             }
-            break;
+            return false;
         case H_42:
             if (record->event.pressed) {
+                SEND_STRING("H_42");
             }
-            break;
+            return false;
         case H_43:
             if (record->event.pressed) {
+                SEND_STRING("H_43");
             }
-            break;
+            return false;
         case H_44:
             if (record->event.pressed) {
+                SEND_STRING("H_44");
             }
-            break;
+            return false;
         case H_45:
             if (record->event.pressed) {
+                SEND_STRING("H_45");
             }
-            break;
+            return false;
         case H_51:
             if (record->event.pressed) {
+                SEND_STRING("R1C3");
             }
-            break;
+            return false;
         case H_52:
             if (record->event.pressed) {
+                SEND_STRING("H_52");
             }
-            break;
+            return false;
         case H_53:
             if (record->event.pressed) {
+                SEND_STRING("H_53");
             }
-            break;
+            return false;
         case H_54:
             if (record->event.pressed) {
+                SEND_STRING("H_54");
             }
-            break;
+            return false;
         case H_55:
             if (record->event.pressed) {
+                SEND_STRING("H_55");
             }
-            break;
+            return false;
+        default:
+            return true;
     }
-    return false;
 }
 
 void matrix_init_user(void) {}
