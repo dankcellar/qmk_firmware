40263bc38b748ec0021f5247e7cf69c637b90288
Eric Downey <edowney29@gmail.com>
2021-07-31 13:03:53 -0400

    5x5

--- keyboards/handleog/x55/keymaps/default/keymap.c
+++ keyboards/handleog/x55/keymaps/default/keymap.c
@@ -4,7 +4,7 @@ enum custom_keycodes { PING = SAFE_RANGE, TURNOFF, TURNON, H_11, H_12, H_13, H_1
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-    [0] = LAYOUT_X55(KC_GESC, H_12, H_13, H_14, H_15, H_21, H_22, H_23, H_24, H_25, H_31, H_32, H_33, H_34, H_35, H_41, H_42, H_43, H_44, H_45, H_51, H_52, H_53, H_54, H_55),
+    [0] = LAYOUT_X55(H_11, H_12, H_13, H_14, H_15, H_21, H_22, H_23, H_24, H_25, H_31, H_32, H_33, H_34, H_35, H_41, H_42, H_43, H_44, H_45, H_51, H_52, H_53, H_54, H_55),
 
     [2] = LAYOUT_X55(KC_GESC, KC_1, KC_2, KC_3, KC_4, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, MO(1)),
 
